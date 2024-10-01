002d9a89be51296fb9c3542e76d3fd02911e4f20
Andreu274 <andreupons@paucasesnovescifp.cat>
2024-10-01 14:05:02 +0200

    funciona

--- ExempleDTOG/src/main/java/ExempleDTO/dto/Client.java
+++ ExempleDTOG/src/main/java/ExempleDTO/dto/Client.java
@@ -1,105 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package ExempleDTO.dto;
-
 
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import javax.swing.JOptionPane;
 
-
+/**
+ *
+ * @author Miquel
+ */
 public class Client {
-    private String DNI;
+    private String dni;
     private String nom;
-    private String cognoms;
+    private String cognom;
     private Date date;
     private String illa;
-    private static Set<String> dnis = new HashSet<>();
+    private static Set<String> dniSet = new HashSet<>();
 
     public Client(String DNI, String nom, String cognoms, Date date, String illa) {
-        if (dnis.contains(DNI)) {
-            JOptionPane.showMessageDialog(null, "El DNI ja existeix: " + DNI, "DNI duplicat", JOptionPane.ERROR_MESSAGE);
+        if (dniSet.contains(DNI)) {
+            JOptionPane.showMessageDialog(null, "El DNI ja existeix: " + DNI , "DNI DUPLICAT", JOptionPane.ERROR_MESSAGE);
             return;
-             
-        } 
+        }
         
-        this.DNI = DNI;
+        this.dni = DNI;
         this.nom = nom;
-        this.cognoms = cognoms;
+        this.cognom = cognoms;
         this.date = date;
         this.illa = illa;
         
-        dnis.add(DNI);
-        
-    }
+        dniSet.add(DNI);
 
-    public Client(String DNI, String nom, String cognoms) {
-            this.DNI = DNI;
-            this.nom = nom;
-            this.cognoms = cognoms;
-            
+    }
+    
+    public Client(String dni, String nom, String cognom) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.dni = dni;
     }
     
 
-
-    public String getDNI() {
-        return DNI;
-    }
-
     public String getNom() {
         return nom;
-    }
-
-    public String getCognoms() {
-        return cognoms;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public String getIlla() {
-        return illa;
-    }
-
-    public void setDNI(String DNI) {
-        this.DNI = DNI;
     }
 
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    public void setCognoms(String cognoms) {
-        this.cognoms = cognoms;
+    public String getCognoms() {
+        return cognom;
+    }
+
+    public void setCognoms(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public Date getDate() {
+        return date;
     }
 
     public void setDate(Date date) {
         this.date = date;
     }
 
+    public String getIlla() {
+        return illa;
+    }
+
     public void setIlla(String illa) {
         this.illa = illa;
     }
 
-    public String[] toArrayString() {
+    public String getDNI() {
+        return dni;
+    }
+
+    public void setDNI(String dni) {
+        this.dni = dni;
+    } 
+    
+    public String[] toArrayString(){
         String[] s = new String[5];
-        s[0] = DNI;
+        s[0] = dni;
         s[1] = nom;
-        s[2] = cognoms;
-        s[3] = (date != null) ? date.toString() : "";
+        s[2] = cognom;
+        s[3] = date.toString();
         s[4] = illa;
         return s;
     }
-
-    @Override
-    public String toString() {
-        return "Client{" + "DNI='" + DNI + '\'' +
-                ", nom='" + nom + '\'' +
-                ", cognoms='" + cognoms + '\'' +
-                ", date=" + date +
-                ", illa='" + illa + '\'' +
-                '}';
-    }
-    
-    
 }
